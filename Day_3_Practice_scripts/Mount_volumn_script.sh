8624dd1da3c05b1f3423eb6191a2d66cf4893d8d
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2024-07-13 00:55:45 +0530

    Update Mount_volumn_script.sh
    1. Pre-requisite note updated
    2. Permission denied issue to store logs is fixed

--- Day_3_Practice_scripts/Mount_volumn_script.sh
+++ Day_3_Practice_scripts/Mount_volumn_script.sh
@@ -1,11 +1,15 @@
-<<Note
-before executing script make sure that EBS volume is created and attached to the instance 
-Note
-
 #!/bin/bash
 
+<<Note
+1.before executing script make sure that EBS volume is created and attached to the instance
+2.make sure EBS_logs folder create or make sure /home/ubuntu/EBS_logs path exist
+Note
+
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-log_file="/root/EBS_logs/log_for_${timestamp}.txt"
+log_file="/home/ubuntu/EBS_logs/log_for_${timestamp}.txt"
+
+#provide write access to folder to store logs
+sudo chmod 744 /home/ubuntu/EBS_logs
 
 #to create file system
 yes | sudo mkfs -t ext4 /dev/xvdf >> $log_file 2>&1
@@ -25,9 +29,9 @@ sudo mount /dev/xvdf /mnt/${foldername} >> $log_file 2>&1
 
 if [ $? -eq 0 ]; then
 
-        echo "Memory sucessfully mounted on $(date)/n
-        /nHardware details:/n/n
-        df -h" >> $log_file 2>&1
-        echo "Memory sucessfully mounted!!" 
+        echo -e "Memory sucessfully mounted on $(date)\n
+        \nHardware details:\n\n
+        $(df -h)" >> $log_file 2>&1
+        echo "Memory sucessfully mounted!!"
 
 fi
